19dad084619b9a5e182ded75e190b85444adc183
sayeedazmal <sayeedazmal01@gmail.com>
2019-09-11 00:47:04 +0600

    Camera Api Add Successfully

--- app/src/main/java/com/example/krishoksomachar/ImageGallery.java
+++ app/src/main/java/com/example/krishoksomachar/ImageGallery.java
@@ -1,15 +1,21 @@
 package com.example.krishoksomachar;
 
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -21,9 +27,17 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.krishoksomachar.Pojoclass.ImgeSourceData;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -42,14 +56,16 @@ import static androidx.constraintlayout.widget.Constraints.TAG;
 public class ImageGallery extends Fragment {
 
 
-    private static final int RESULT_OK = 1;
+
 
     public ImageGallery() {
 
     }
 
-    private Context context;
 
+
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int REQUEST_STORAGE_ACCESS = 2;
 
     private static final int REQUEST_CODE = 1;
     private boolean mStoragePermissions;
@@ -57,46 +73,127 @@ public class ImageGallery extends Fragment {
     private GalleryimgAdapter adapter;
     private RecyclerView rec;
     private File[] imageList;
+    private Context context;
+
+    private ImageView iv;
+    private File imageFile = null;
+    private String filePath;
+    private int requestCode,resultCode;
+    private Intent data;
 
 
     @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        this.context = context;
+    }
+    public ImageGallery(int requestCode, int resultCode, Intent data){
+        this.requestCode = requestCode;
+        this.resultCode = resultCode;
+        this.data = data;
+    }
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+         View view = inflater.inflate(R.layout.fragment_image_gallery, container, false);
+         iv = view.findViewById(R.id.iv);
+         try {
+            Picasso.get().load(String.valueOf(data)).placeholder(R.drawable.flower).into(iv);
+             Toast.makeText(getActivity(),"error"+data,Toast.LENGTH_LONG).show();
+         }catch (Exception e){
+             Toast.makeText(getActivity(),"error"+e,Toast.LENGTH_LONG).show();
+         }
 
-        View view = inflater.inflate(R.layout.fragment_image_gallery, container, false);
-        rec = view.findViewById(R.id.imgRecycler);
-        rec.setLayoutManager(new GridLayoutManager(getActivity(),2));
-        adapter = new GalleryimgAdapter(getActivity(), getData());
-        rec.setAdapter(adapter);
 
         return view;
     }
 
-    public List<ImgeSourceData> getData(){
 
-        imageLists = new ArrayList<>();
+//            @Override
+//            public void onActivityResult(int requestCode, int resultCode, Intent data) {
+//                super.onActivityResult(requestCode, resultCode, data);
+//                if(requestCode == REQUEST_IMAGE_CAPTURE &&
+//                        resultCode == Activity.RESULT_OK){
+//                    //start progressbar
+//
+//
+////            final StorageReference rootRef =
+////                    FirebaseStorage.getInstance().getReference();
+////            Uri fileUri = Uri.fromFile(imageFile);
+////            final StorageReference photoRef =
+////                    rootRef.child("images/"+fileUri.getLastPathSegment());
+////
+////            UploadTask uploadTask = photoRef.putFile(fileUri);
+////
+////            Task<Uri> uriTask = uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+////                @Override
+////                public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+////                    if(!task.isSuccessful()){
+////                        throw task.getException();
+////                    }
+////                    return photoRef.getDownloadUrl();
+////                }
+////            });
+////            uriTask.addOnCompleteListener(new OnCompleteListener<Uri>() {
+////                @Override
+////                public void onComplete(@NonNull Task<Uri> task) {
+////                    Uri downloadUri = task.getResult();
+////                    Picasso.get().load(downloadUri).into(iv);
+////                }
+////            });
+//
+//        }
+//    }
 
-        File downloadFolder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
-        ImgeSourceData imgsData;
-        try {
-            if (downloadFolder.exists()) {
-                File[] files = downloadFolder.listFiles();
 
-                for (int i = 0; i <files.length; i++) {
-                    File file = files[i];
-                    imgsData = new ImgeSourceData();
-                    imgsData.setUri(Uri.fromFile(file));
-                    imageLists.add(imgsData);
-                }
-            }
-        }catch(Exception e){
+//
+//    public void setPic() {
+//        // Get the dimensions of the View
+//        int targetW = iv.getWidth();
+//        int targetH = iv.getHeight();
+//
+//        // Get the dimensions of the bitmap
+//        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+//        bmOptions.inJustDecodeBounds = true;
+//        BitmapFactory.decodeFile(filePath, bmOptions);
+//        int photoW = bmOptions.outWidth;
+//        int photoH = bmOptions.outHeight;
+//
+//        // Determine how much to scale down the image
+//        int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+//
+//        // Decode the image file into a Bitmap sized to fill the View
+//        bmOptions.inJustDecodeBounds = false;
+//        bmOptions.inSampleSize = scaleFactor;
+//        //bmOptions.inPurgeable = true;
+//
+//        Bitmap bitmap = BitmapFactory.decodeFile(filePath, bmOptions);
+//        iv.setImageBitmap(bitmap);
+//    }
 
-                Toast.makeText(getActivity(), "error " + e, Toast.LENGTH_SHORT).show();
-            }
 
-
-
-        return imageLists;
-    }
+//
+//    public List<ImgeSourceData> getData(){
+//
+//        imageLists = new ArrayList<>();
+//
+//        File folder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+//        ImgeSourceData imgsData;
+//        try {
+//            if (folder.exists()) {
+//                File[] files = folder.listFiles();
+//                for (int i = 0; i <files.length; i++) {
+//                    File file = files[i];
+//                    imgsData = new ImgeSourceData();
+//                    imgsData.setUri(Uri.fromFile(file));
+//                    imageLists.add(imgsData);
+//                }
+//            }
+//        }catch(Exception e){
+//
+//                Toast.makeText(getActivity(), "error " + e, Toast.LENGTH_SHORT).show();
+//            }
+//        return imageLists;
+//    }
 
 }
--- app/src/main/java/com/example/krishoksomachar/Navhome.java
+++ app/src/main/java/com/example/krishoksomachar/Navhome.java
@@ -1,44 +1,62 @@
 package com.example.krishoksomachar;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.FileProvider;
 import androidx.core.view.GravityCompat;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import android.view.MenuItem;
+
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-import androidx.recyclerview.widget.RecyclerView;
 
+import androidx.fragment.app.FragmentManager;
 import android.view.Menu;
 import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.Toast;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-import java.util.ArrayList;
 
 public class Navhome extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener
         ,Cropdetails.Cropdetailsinterface
         ,Cropchild.onauthCropchild
         ,Cropdises.ondeasesinterface{
 
+    private static final int REQUEST_IMAGE_CAPTURE =1 ;
+    private static final int REQUEST_STORAGE_ACCESS =2 ;
     private Button cropbtn;
-    private ImageButton camerabtn;
     private Context context;
     private FragmentManager manager;
     private Fragment fragment = null;
+
+
+    private File imageFile = null;
+    private String filePath;
+
 
 
     @Override
@@ -53,17 +71,36 @@ public class Navhome extends AppCompatActivity implements NavigationView.OnNavig
         if(fragment!=null){
             manager.beginTransaction().replace(R.id.content_fragment,fragment).commit();
         }
-
+        checkStoragePermission();
         FloatingActionButton fab = (FloatingActionButton)findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action",
-//                        Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-                manager.beginTransaction().replace(R.id.content_fragment,new ImageGallery()).addToBackStack("nav").commit();
+
+
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if(intent.resolveActivity(Navhome.this.getPackageManager())
+                        != null){
+                    try {
+                        imageFile = createImageFile();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    if(imageFile != null){
+                        Uri fileUri = FileProvider.getUriForFile(Navhome.this,
+                                "com.example.krishoksomachar",
+                                imageFile);
+                        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+                        startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+                    }
+
+                }
             }
+
+
         });
+
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
@@ -74,6 +111,64 @@ public class Navhome extends AppCompatActivity implements NavigationView.OnNavig
 
     }
 
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQUEST_IMAGE_CAPTURE &&
+                resultCode == Activity.RESULT_OK){
+
+            final StorageReference rootRef =
+                    FirebaseStorage.getInstance().getReference();
+            Uri fileUri = Uri.fromFile(imageFile);
+            final StorageReference photoRef =
+                    rootRef.child("images/"+fileUri.getLastPathSegment());
+
+            UploadTask uploadTask = photoRef.putFile(fileUri);
+            Task<Uri> uriTask = uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                @Override
+                public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                    if(!task.isSuccessful()){
+                        throw task.getException();
+                    }
+                    return photoRef.getDownloadUrl();
+                }
+            });
+            uriTask.addOnCompleteListener(new OnCompleteListener<Uri>() {
+                @Override
+                public void onComplete(@NonNull Task<Uri> task) {
+                    Uri downloadUri = task.getResult();
+                    //stop progressbar
+//                    Picasso.get().load(downloadUri).into(iv);
+                }
+            });
+        }
+    }
+
+
+    private boolean checkStoragePermission(){
+        if(ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) !=
+                PackageManager.PERMISSION_GRANTED){
+                ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    REQUEST_STORAGE_ACCESS);
+            return false;
+        }
+
+        return true;
+    }
+
+    private File createImageFile()  throws IOException{
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_hhmmss").format(new Date());
+        String imageFileName = "JPEG_"+timeStamp;
+        File dir = this.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File file = File.createTempFile(imageFileName, ".jpg", dir);
+        filePath = file.getAbsolutePath();
+        return file;
+    }
+
+//Navigation work
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
